3cc13bcb170b7de66f144e7b7ca5caa8303bdaf6
AsandyPutra77 <2110817310002@mhs.ulm.ac.id>
2024-05-28 20:43:31 +0800

    Login regis (#2)
    * add background for login and refactor
    
    * add background for register and add icon
    
    ---------
    
    Co-authored-by: mhmmdhffzz <mhmmdhffzz@gmail.com>

--- src/components/auth/LoginInput.jsx
+++ src/components/auth/LoginInput.jsx
@@ -10,111 +10,138 @@ import {
   Heading,
   Text,
   useColorModeValue,
-  useToast
+  useToast,
+  Grid,
+  GridItem,
+  Image,
+  Icon
 } from '@chakra-ui/react';
 
-import {Link as ReactRouterLink } from 'react-router-dom';
-import { useNavigate} from 'react-router-dom';
+import { Link as ReactRouterLink } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../firebase/firebase';
+import { FaEnvelope, FaLock } from 'react-icons/fa'; // Import envelope and lock icons
 
 export const LoginInput = () => {
-const [ email, setEmail ] = useState('');
-const [ password, setPassword ] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-const toast = useToast();
+  const toast = useToast();
+  const navigate = useNavigate();
 
-const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      console.log("User Logged In Successfully !");
+      toast({
+        title: 'Login successful.',
+        description: "You've successfully logged in.",
+        status: 'success',
+        duration: 4000,
+        isClosable: true,
+        onCloseComplete: () => navigate('/landing')
+      });
+    } catch (error) {
+      console.log(error.message);
+      toast({
+        title: 'Login failed.',
+        description: "Email or password is incorrect.",
+        status: 'error',
+        duration: 9000,
+        isClosable: true,
+      });
+    }
+  };
 
-const handleSubmit = async (e) => {
-  e.preventDefault();
-  try {
-    await signInWithEmailAndPassword(auth, email, password);
-    console.log("User Logged In Successfully !");
-    toast({
-      title: 'Login successful.',
-      description: "You've successfully logged in.",
-      status: 'success',
-      duration: 4000,
-      isClosable: true,
-      onCloseComplete: () => navigate('/landing')
-    })
-
-  } catch (error) {
-    console.log(error.message)
-    toast({
-      title: 'Login failed.',
-      description: "Email or password is incorrect.",
-      status: 'error',
-      duration: 9000,
-      isClosable: true,
-    })
-  }
-}
-  
-return (
-  <Flex
-    minH={'100vh'}
-    align={'center'}
-    justify={'center'}
-    bg={useColorModeValue('gray.50', 'gray.800')}>
-    <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-      <Stack align={'center'}>
-        <Heading fontSize={'4xl'}>Sign in to your account</Heading>
-        <Text fontSize={'lg'} color={'gray.600'}>
-          Don&apos;t have an account ? <Link as={ReactRouterLink} to="/register" color={'blue.400'}>Register</Link>✌️
-        </Text>
-      </Stack>
-      <Box
-        rounded={'lg'}
-        bg={useColorModeValue('white', 'gray.700')}
-        boxShadow={'lg'}
-        p={8}>
-        <form onSubmit={handleSubmit}>
-        <Stack spacing={4}>
-          <FormControl id="email">
-            <FormLabel>Email address</FormLabel>
-            <Input type="email"
-               className='form-control'
-               placeholder='Masukkan Email Anda...'
-               value={email}
-               onChange={(e) => setEmail(e.target.value)}
-               />
-          </FormControl>
-          <FormControl id="password">
-            <FormLabel>Password</FormLabel>
-            <Input type="password"
-               className='form-control'
-               placeholder='Masukkan Password Anda...'
-               value={password}
-               onChange={(e) => setPassword(e.target.value)}
-               />
-          </FormControl>
-          <Stack spacing={10}>
-            <Stack
-              direction={{ base: 'column', sm: 'row' }}
-              align={'start'}
-              justify={'space-between'}>
-              <Button colorScheme="red" w={'full'} onClick={() => {/* Add your Google sign-in logic here */}}>
-                Sign in with Google 🌍
-              </Button>
+  return (
+    <Grid
+      minH={'100vh'}
+      templateColumns={{ base: '1fr', md: '1fr 1fr' }}
+      bg={useColorModeValue('gray.50', 'gray.800')}
+    >
+      <GridItem>
+        <Flex align={'center'} justify={'center'} h="100%">
+          <Box
+            bg="#C6F6D5"
+            width="100%"
+            height="100%"
+            position="relative"
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Image
+              src="/assets/Group 1.png"
+              alt="Group 1"
+              objectFit="center"
+              w="60%"
+              h="50%"
+            />
+          </Box>
+        </Flex>
+      </GridItem>
+      <GridItem>
+        <Flex align={'center'} justify={'center'} h="100%">
+          <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+            <Stack align={'center'}>
+              <Heading fontSize={'4xl'}>Sign in to your account</Heading>
+              <Text fontSize={'lg'} color={'gray.600'}>
+                Don&apos;t have an account? <Link as={ReactRouterLink} to="/register" color={'blue.400'}>Register</Link>
+              </Text>
             </Stack>
-            <Button
-              type='submit'
-              bg={'blue.400'}
-              color={'white'}
-              //onClick={ () => navigate('/landing')}
-              _hover={{
-                bg: 'blue.500',
-              }}>
-              Sign in
-            </Button>
+            <Box
+              rounded={'lg'}
+              bg={useColorModeValue('white', 'gray.700')}
+              boxShadow={'lg'}
+              p={8}
+            >
+              <form onSubmit={handleSubmit}>
+                <Stack spacing={4}>
+                  <FormControl id="email">
+                    <FormLabel>Email address</FormLabel>
+                    <Flex align="center">
+                      <Icon as={FaEnvelope} color="gray.400" />
+                      <Input
+                        type="email"
+                        className='form-control'
+                        placeholder='Masukkan Email Anda...'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                    </Flex>
+                  </FormControl>
+                  <FormControl id="password">
+                    <FormLabel>Password</FormLabel>
+                    <Flex align="center">
+                      <Icon as={FaLock} color="gray.400" />
+                      <Input
+                        type="password"
+                        className='form-control'
+                        placeholder='Masukkan Password Anda...'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                      />
+                    </Flex>
+                  </FormControl>
+                  <Button
+                    type='submit'
+                    bg={'blue.400'}
+                    color={'white'}
+                    _hover={{
+                      bg: 'blue.500',
+                    }}
+                  >
+                    Sign in
+                  </Button>
+                </Stack>
+              </form>
+            </Box>
           </Stack>
-        </Stack>
-        </form>
-      </Box>
-    </Stack>
-  </Flex>
-);
+        </Flex>
+      </GridItem>
+    </Grid>
+  );
 }
